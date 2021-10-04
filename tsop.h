9cef5f1ac4aa739f972b134028c1743325409d9d
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2021-10-04 23:42:45 +0900

    Create tsop.h

---
+++ tsop.h
@@ -0,0 +1,13 @@
+/*
+    tsop.h - operators for struct timespec in time.h
+    
+    20211004 Created
+*/
+#include <time.h>
+
+/* functions */
+int tsop_subtract
+( const struct timespec *ts1
+, const struct timespec *ts2
+, struct timespec *ts3 
+) ;
