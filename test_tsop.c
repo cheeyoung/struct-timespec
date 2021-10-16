e16db60f93c362b68f334bf9c5df187efa01ce1d
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2021-10-16 15:24:50 +0900

    Update test_tsop.c

--- test_tsop.c
+++ test_tsop.c
@@ -34,12 +34,20 @@ int main()
     printf("Elapsed Time : %ld.%09ld \n", ts3.tv_sec, ts3.tv_nsec) ;
 
     ts2.tv_sec = 1 ; ts2.tv_nsec = 1 ;  /* 000,000,001 */
-    printf("DEBUG: %ld.%09ld \n", ts2.tv_sec, ts2.tv_nsec) ;
+    printf("INFO: %ld.%09ld \n", ts2.tv_sec, ts2.tv_nsec) ;
     ts1.tv_sec = 0 ; ts1.tv_nsec = 999999999 ;  /* 999,999,999 */
-    printf("DEBUG: %ld.%09ld \n", ts1.tv_sec, ts1.tv_nsec) ;
+    printf("INFO: %ld.%09ld \n", ts1.tv_sec, ts1.tv_nsec) ;
 
     rv = tsop_subtract(&ts2, &ts1, &ts3) ;
     printf("Elapsed Time : %ld.%09ld \n", ts3.tv_sec, ts3.tv_nsec) ;
+
+    ts2.tv_sec = 0 ; ts2.tv_nsec = 999999999 ;  /* 999,999,999 */
+    printf("INFO: %ld.%09ld \n", ts2.tv_sec, ts2.tv_nsec) ;
+    ts1.tv_sec = 1 ; ts1.tv_nsec = 2 ;  /* 000,000,002 */
+    printf("INFO: %ld.%09ld \n", ts1.tv_sec, ts1.tv_nsec) ;
+
+    rv = tsop_add(&ts2, &ts1, &ts3) ;
+    printf("Total : %ld.%09ld \n", ts3.tv_sec, ts3.tv_nsec) ;
 
     return 0 ;
 }
