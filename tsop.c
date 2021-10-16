19306af973de2d35158c1ef3274e9217be9b6991
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2021-10-16 15:23:59 +0900

    Update tsop.c

--- tsop.c
+++ tsop.c
@@ -37,3 +37,20 @@ int tsop_subtract
 
     return 0 ;
 }
+
+int tsop_add
+( const struct timespec *ts1
+, const struct timespec *ts2
+, struct timespec *ts3
+)
+{
+    (*ts3).tv_sec = (*ts1).tv_sec + (*ts2).tv_sec ;
+    (*ts3).tv_nsec = (*ts1).tv_nsec + (*ts2).tv_nsec ;
+    if ( (*ts3).tv_nsec > 999999999 )
+    {
+        (*ts3).tv_sec = (*ts3).tv_sec + 1 ;
+        (*ts3).tv_nsec = (*ts3).tv_nsec - 1000000000 ;  /* 1,000,000,000 */
+    }
+
+    return 0 ;
+}
