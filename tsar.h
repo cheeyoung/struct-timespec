c3323e20be94f0623e6a0716716a511a21797e4b
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2021-10-23 02:27:44 +0900

    Update tsar.h

--- tsar.h
+++ tsar.h
@@ -7,11 +7,13 @@
 
 typedef struct
 {
-    int n_elements ;  /* number of the elements, length of the array in array index */
-    int n_length ;  /* length of the elements in byte */
-    struct timespec *p ;
+    int n_offset ;  /* number of the elements to skip */
+    int n_elements ;  /* number of the elements in the array, length of the array in array index */
+    int n_length ;  /* length of the elements(struct timespec) in byte */
+    struct timespec *p ;  /* void *p */
 }   tsarr ;
 
 /* functions */
+int tsar_gettime(tsarr *ta) ;
 int tsar_malloc(tsarr *ta, const int n) ;
 int tsar_free(tsarr *ta) ;
