1a09426500a00f0740d3c374291ae3722f0aa72c
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2021-10-22 20:51:27 +0900

    Update tsar.h

--- tsar.h
+++ tsar.h
@@ -1,9 +1,17 @@
 /*
     array of struct timespec
     
+    20211022 typedef struct tsarr
     20210821 Created
 */
 
+typedef struct
+{
+    int n_elements ;  /* number of the elements, length of the array in array index */
+    int n_length ;  /* length of the elements in byte */
+    struct timespec *p ;
+}   tsarr ;
+
 /* functions */
-struct timespec *tsar_malloc(const int n) ;
-int tsar_free(struct timespec *ts) ;
+int tsar_malloc(tsarr *ta, const int n) ;
+int tsar_free(tsarr *ta) ;
