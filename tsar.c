41f13bd0dc77b20a26d746c4679af48d574a681c
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2021-10-22 20:54:53 +0900

    Update tsar.c

--- tsar.c
+++ tsar.c
@@ -23,7 +23,7 @@ int tsar_malloc(tsarr *ta, const int n)
     if ((*ta).p == 0)
     {
         printf("ERROR: malloc returned 0, errno = %d\n", errno) ;
-        return 0 ;
+        return -1 ;
     }
     printf("DEBUG: malloc returned 0x%lx\n", (unsigned long int)p) ;
     return 0 ;
