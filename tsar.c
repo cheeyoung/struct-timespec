e9dfaa28ba44222f07617375d262134e34dd06f0
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2021-10-22 20:53:16 +0900

    Update tsar.c

--- tsar.c
+++ tsar.c
@@ -1,30 +1,37 @@
 /*
     array of struct timespec
-
+    
+    20211022 typedef struct tsarr
     20210821 Created
 */
 #include <time.h>
 #include <stdlib.h>  /* malloc */
 #include <stdio.h>
+#include "tsar.h"
 
-struct timespec *tsar_malloc(const int n)
+int tsar_malloc(tsarr *ta, const int n)
 {
     void *p ;
     int errno ;  /* Linux */
     
+    (*ta).n_elements = n ;
+    
     printf("DEBUG: size of struct timespec is %ld\n", sizeof(struct timespec)) ;
-    p = malloc(sizeof(struct timespec) * n) ;
-    if (p == 0)
+    (*ta).n_length = sizeof(struct timespec) ;
+    
+    (*ta).p = malloc((*ta).n_length * (*ta).n_elements) ;
+    if ((*ta).p == 0)
     {
         printf("ERROR: malloc returned 0, errno = %d\n", errno) ;
         return 0 ;
     }
     printf("DEBUG: malloc returned 0x%lx\n", (unsigned long int)p) ;
-    return (struct timespec *)p ;
+    return 0 ;
 }
 
-int tsar_free(struct timespec *ts)
+int tsar_free(tsarr *ta)
 {
-    free(ts) ;
+    free((*ta).p) ;
+    (*ta).p = 0 ;
     return 0 ;
 }
