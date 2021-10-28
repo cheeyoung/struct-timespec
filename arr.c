0d68dc0bbfbdc05022ec12f060d6d87fef154276
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2021-10-28 20:36:19 +0900

    Create arr.c

---
+++ arr.c
@@ -0,0 +1,52 @@
+/*
+    20211028 Created
+*/
+#include <stdlib.h>  /* malloc */
+#include <string.h>  /* strlen */
+#include <stdio.h>
+#include "arr.h"
+
+/* functions */
+int arr_dump(arr *a)
+{
+    printf("Array %lx\n", (unsigned long int)(*a).p) ;
+    printf("The number of elements = %d\n", (*a).al) ;
+    printf("The length of each element = %d for type \"%s\"\n", (*a).el, (*a).typestr) ;
+    return 0 ;
+}
+
+int arr_malloc(arr *a, const int al, const int el, const char *typestr)
+{
+    size_t len ;
+    int errno ;  /* Linux */
+    
+    (*a).al = al ; (*a).el = el ;
+    
+    len = strlen(typestr) ;
+    len = len + 1 ;  /* for null */
+    (*a).typestr = malloc(len) ;
+    if ((*a).typestr == 0)
+    {
+        printf("ERROR: malloc returned 0 for %ld, errno = %d\n", len, errno) ;
+        return -1 ;
+    }
+    printf("DEBUG: malloc returned %lx for %ld\n", (unsigned long int)(*a).typestr, len) ;
+    strcpy((*a).typestr, typestr) ;
+    
+    len = (*a).el * (*a).al ;
+    (*a).p = malloc(len) ;
+    if ((*a).p == 0)
+    {
+        printf("ERROR: malloc returned 0 for %ld, errno = %d\n", len, errno) ;
+        return -1 ;
+    }
+    printf("DEBUG: malloc returned %lx for %ld\n", (unsigned long int)(*a).p, len) ;
+    return 0 ;
+}
+
+int arr_free(arr *a)
+{
+    free((*a).p) ;
+    (*a).p = 0 ;
+    return 0 ;
+}
