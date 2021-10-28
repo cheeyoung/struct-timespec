f24f30f80c93f21012fa8e3739cbfbae8e888ff2
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2021-10-28 19:31:30 +0900

    Create arr.h

---
+++ arr.h
@@ -0,0 +1,14 @@
+/*
+    20211028 Created
+*/
+typedef struct
+{
+    int al ;  /* length of the array, in array index */
+    int el ;  /* length of each element, in byte */
+    char *typestr ;
+    void *p ;
+}   arr ;
+
+/* functions */
+int arr_malloc(arr *a, const int al, const int el, const char *typestr) ;
+int arr_free(arr *a) ;
