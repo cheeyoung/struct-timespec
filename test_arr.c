8c786737cf5e243762d10c6452a283ac1cde0731
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2021-10-31 00:09:09 +0900

    Update test_arr.c

--- test_arr.c
+++ test_arr.c
@@ -7,21 +7,29 @@
 
 int main()
 {
-    arr a1, a2, a3 ;
+    arr a1, a2, a3, a4, a5 ;
     
     int rv ;  /* Return Value */
     
-    rv = arr_malloc(&a1, 256, sizeof(int), "int") ;
+    rv = arr_malloc(&a1, 512, sizeof(int), "int") ;
     printf("arr_malloc returned %d\n", rv) ;
     rv = arr_dump(&a1) ;
     
-    rv = arr_malloc(&a2, 256, sizeof(double), "double") ;
+    rv = arr_malloc(&a2, 256, sizeof(long int), "long int") ;
     printf("arr_malloc returned %d\n", rv) ;
     rv = arr_dump(&a2) ;
     
-    rv = arr_malloc(&a3, 256, sizeof(struct timespec), "struct timespec") ;
+    rv = arr_malloc(&a3, 512, sizeof(float), "float") ;
     printf("arr_malloc returned %d\n", rv) ;
     rv = arr_dump(&a3) ;
+    
+    rv = arr_malloc(&a4, 256, sizeof(double), "double") ;
+    printf("arr_malloc returned %d\n", rv) ;
+    rv = arr_dump(&a4) ;
+    
+    rv = arr_malloc(&a5, 128, sizeof(struct timespec), "struct timespec") ;  /* 2048 = 2KB */
+    printf("arr_malloc returned %d\n", rv) ;
+    rv = arr_dump(&a5) ;
 
     for (int i = 0; i < 256; i++)
         ((int *)a1.p)[i] = i ;
@@ -36,6 +44,12 @@ int main()
     printf("arr_free returned %d\n", rv) ;
 
     rv = arr_free(&a3) ;
+    printf("arr_free returned %d\n", rv) ;
+
+    rv = arr_free(&a4) ;
+    printf("arr_free returned %d\n", rv) ;
+
+    rv = arr_free(&a5) ;
     printf("arr_free returned %d\n", rv) ;
 
     return 0 ;
