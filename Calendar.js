1f6436f08b4c6a77dae2dc94db088f1456165089
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2022-01-25 20:40:06 +0900

    Create Calendar.js

---
+++ Calendar.js
@@ -0,0 +1,9 @@
+/*
+  23 Jan 2022 Created for getDay() and getMonth() methods of the Date object
+*/
+// Names of the days of the week
+let dow = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'] ;
+let dow3 = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'] ;
+// Names of the months
+let mon = ['January','February','March','April','May','June','July','August','September','October','November','December'] ;
+let mon3 = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'] ;
