8bd4e7aabf81ed4a702dfbb291db6435b066d016
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2022-02-23 23:31:18 +0900

    Added logAppendLine()

--- Log.js
+++ Log.js
@@ -6,7 +6,7 @@
    allocation and initialization of the pre element
    Used var for Safari on iOS
 */
-var pre01 = document.createElement('pre') ;
+var pre01 = document.createElement('pre') ;  // an HTMLElement
 pre01.id = "log" ;
 pre01.class = "width: 300px" ;
 
@@ -14,3 +14,8 @@ pre01.class = "width: 300px" ;
    insert the pre element to the section
 */
 document.getElementById('section-log').insertBefore(pre01, document.getElementById('script-log')) ;
+
+function logAppendLine(he, str)  // an HTMLElement and a string
+{
+   he.innerText += 'INFO: ' + str + '\n' ;
+}
